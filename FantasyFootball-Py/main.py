91140724001a4373649f50562fd73640cc693ef1
CamHaynes18 <camhaynes18@gmail.com>
2022-11-30 19:52:03 -0500

    Update Python packages

--- FantasyFootball-Py/main.py
+++ FantasyFootball-Py/main.py
@@ -17,6 +17,6 @@ print('requirements.txt Saved')
 #    print('Files Backed Up')
 
 # Call script
-exec(open('src/prospectGrade.py').read())
+#exec(open('src/prospectGrade.py').read())
 
 print('Script Complete')
